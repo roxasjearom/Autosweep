9bfa5c7efe3ce86d408f93491e5b6a33cd30ae02
Jearom Roxas <roxasjearom@gmail.com>
2025-06-27 19:41:37 +0800

    [AR-29] Implement basic navigation using Navigation 3

---
+++ app/src/main/java/com/loraxx/electrick/autosweep/navigation/Route.kt
@@ -0,0 +1,10 @@
+package com.loraxx.electrick.autosweep.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object Login : NavKey
+
+@Serializable
+data object QuickBalance : NavKey
