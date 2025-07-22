19b85116caae9f4679db38250340c583718780b3
Jearom Roxas <roxasjearom@gmail.com>
2025-07-22 13:33:50 +0800

    [AR-48] Add traffic advisory to Dashboard screen

--- app/src/main/java/com/loraxx/electrick/autosweep/domain/repository/DashboardRepository.kt
+++ app/src/main/java/com/loraxx/electrick/autosweep/domain/repository/DashboardRepository.kt
@@ -1,7 +1,10 @@
 package com.loraxx.electrick.autosweep.domain.repository
 
 import com.loraxx.electrick.autosweep.domain.model.BalanceDetails
+import com.loraxx.electrick.autosweep.domain.model.TrafficAdvisory
 
 interface DashboardRepository {
     suspend fun getBalanceDetails(accountNumber: String): BalanceDetails
+
+    suspend fun getTrafficAdvisory(): TrafficAdvisory
 }
